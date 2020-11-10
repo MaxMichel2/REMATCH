353d14a4f40bb40ae2625e797e960abbbfb28e1d
Remi1997 <remi-1997@live.fr>
2020-11-10 12:48:49 +0100

    Retrait des boutons pour le scan

--- app/src/main/java/com/rematch/aroundme/InterfaceActivity.java
+++ app/src/main/java/com/rematch/aroundme/InterfaceActivity.java
@@ -28,95 +28,79 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import java.util.List;
 
 public class InterfaceActivity extends AppCompatActivity {
-        private Button captureImageBtn, detectTextBtn;
-        private ImageView imageView;
-        private TextView textView;
-        static final int REQUEST_IMAGE_CAPTURE = 1;
-        private Bitmap imageBitmap;
+    private ImageView imageView;
+    private TextView textView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_interface);
-            FirebaseApp.initializeApp(InterfaceActivity.this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface);
+        FirebaseApp.initializeApp(InterfaceActivity.this);
 
-            captureImageBtn = findViewById(R.id.capture_image_btn);
-            detectTextBtn = findViewById(R.id.detect_text_image_btn);
-            imageView = findViewById(R.id.image_view);
-            textView = findViewById(R.id.text_display);
-
-            captureImageBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    dispatchTakePictureIntent();
-                    textView.setText("");
-                }
-            });
-            detectTextBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    detectTextFromImage();
-                }
-            });
-        }
-
-        private void dispatchTakePictureIntent()
-        {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null){
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data)
-        {
-            super.onActivityResult(requestCode, resultCode, data);
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                imageBitmap = (Bitmap) extras.get("data");
-                imageView.setImageBitmap(imageBitmap);
-            }
-        }
-        private void detectTextFromImage()
-        {
-            FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
-            FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
-            firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-                @Override
-                public void onSuccess(FirebaseVisionText firebaseVisionText)
-                {
-                    displayTextFromImage(firebaseVisionText );
-
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e)
-                {
-                    Toast.makeText(InterfaceActivity.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
-
-                    Log.d("Error: ", e.getMessage());
-
-                }
-            });
-        }
-
-        private void displayTextFromImage(FirebaseVisionText firebaseVisionText)
-        {
-            List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
-            if (blockList.size()==0){
-                Toast.makeText(this, "No text found in Image.", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()){
-                    String text = block.getText();
-                    textView.setText(text);
-                }
-            }
-        }
-
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.text_display);
+        dispatchTakePictureIntent();
+        textView.setText("");
     }
+
+    private void dispatchTakePictureIntent()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            detectTextFromImage();
+        }
+    }
+    private void detectTextFromImage()
+    {
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText)
+            {
+                displayTextFromImage(firebaseVisionText );
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                Toast.makeText(InterfaceActivity.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                Log.d("Error: ", e.getMessage());
+
+            }
+        });
+    }
+
+    private void displayTextFromImage(FirebaseVisionText firebaseVisionText)
+    {
+        List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
+        if (blockList.size()==0){
+            Toast.makeText(this, "No text found in Image.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks()){
+                String text = block.getText();
+                textView.setText(text);
+            }
+        }
+    }
+
+}
