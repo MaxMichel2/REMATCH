4047905d26213815c41eb64d38b2bf0e397d4be8
Theotime <theotime.balaguer@gmail.com>
2020-11-26 17:47:14 +0100

    GPS fix

--- app/src/main/java/com/rematch/aroundme/APImapsActivity.java
+++ app/src/main/java/com/rematch/aroundme/APImapsActivity.java
@@ -5,15 +5,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -25,14 +29,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
@@ -68,27 +80,39 @@ public class APImapsActivity extends AppCompatActivity {
     SupportMapFragment supportMapFragment;
     GoogleMap map;
     AlertDialog.Builder builder;
-    FusedLocationProviderClient client;
+
+    // Partie Places
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
     double currentLat = 0, currentLong = 0;
+    List<HashMap<String, String>> possiblePlaces = null;
+    int LOCATION_REQUEST_CODE = 10001;
+    private static final String TAG = "InterfaceActivity";
+    LocationCallback locationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if(locationResult == null){
+                return;
+            }
+            for(Location location: locationResult.getLocations()){
+                Log.d(TAG, "onLocationResults: " + location.toString());
+                currentLat = location.getLatitude();
+                currentLong = location.getLongitude();
+                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        // When map is ready
+                        map = googleMap;
+                        // Zoom current location on map
+                        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(currentLat, currentLong)));
+                    }
+                });
+            }
+        }
+    };
 
     private TextToSpeech tts;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Check permissions
-        if (ActivityCompat.checkSelfPermission(APImapsActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // When permission is granted
-            // Call getCurrentLocation
-            getCurrentLocation();
-        } else {
-            // When permission is denied
-            // Request permission
-            ActivityCompat.requestPermissions(APImapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,46 +141,13 @@ public class APImapsActivity extends AppCompatActivity {
                 .findFragmentById(R.id.google_map);
 
         // Initialise fused location provider client
-        client = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(4000);
+        locationRequest.setFastestInterval(2000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         builder= new AlertDialog.Builder(this);
-        // Initialise array of place type
-        //final String[] placeTypeList = {"atm", "bank", "hospital", "movie_theater", "restaurant", "store"};
-        // Initialise array of place names
-        //String[] placeNameList = {"ATM", "Bank", "Hospital", "Movie Theater", "Restaurant", "Store"};
-
-        // Set adapter for Spinner
-        //spType.setAdapter(new ArrayAdapter<String>(APImapsActivity.this
-                //, android.R.layout.simple_spinner_dropdown_item, placeNameList));
-        /*
-        btFind.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Get selected position of spinner
-                int i = spType.getSelectedItemPosition();
-                // Initialize url
-                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" + //Url
-                        "?location=" + currentLat + "," + currentLong + //Location latitude and longitude
-                        "&radius=500" + //Nearby radius
-                        "&types=" + placeTypeList[i] + //place type
-                        "&sensor=true" + //sensor
-                        "&key=" + getResources().getString(R.string.google_map_key); //Google map key
-
-                // Execute place task method to download json data
-                new APImapsActivity.PlaceTask().execute(url);
-            }
-        });
-
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    tts.setLanguage(Locale.FRANCE);
-                }
-            }
-        });
-
-        */
 
         btATM.setOnClickListener(new View.OnClickListener() {
 
@@ -324,6 +315,34 @@ public class APImapsActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Check permissions
+        if (ActivityCompat.checkSelfPermission(APImapsActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // When permission is granted
+            // Call checkSettingsAndStartLocationUpdates
+            checkSettingsAndStartLocationUpdates();
+        } else {
+            // When permission is denied
+            // Request permission
+            askLocationPermission();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
+
     public boolean onKeyDown(int keyCode, KeyEvent event)
     {
         if ((keyCode == KeyEvent.KEYCODE_BACK))
@@ -334,76 +353,56 @@ public class APImapsActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    private void getCurrentLocation() {
-        // Initialise Task<Location>
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-
-        // TODO: Change the fused Location call to a continuous GPS call (subscribe to "requestLocationUpdates" method)
-        //      For now, if the GPS has no fix at the opening of the app, we get 0.0, 0.0 as location.
-
-        Task<Location> task = client.getLastLocation();
-
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+    private void checkSettingsAndStartLocationUpdates(){
+        LocationSettingsRequest request = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> locationSettingsResponseTask = client.checkLocationSettings(request);
+        locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
             @Override
-            public void onSuccess(final Location location) {
-                // When successful
-                if (location != null){
-                    // Get current latitude
-                    currentLat = location.getLatitude();
-                    // Get current longitude
-                    currentLong = location.getLongitude();
-                    // Sync map
-                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            // When map is ready
-                            map = googleMap;
-
-                            // Initialise LatLng
-                            LatLng latLng = new LatLng(currentLat, currentLong);
-
-//                            // Initialize fake lat lng for development purpose
-//                            currentLat = 45.763850;
-//                            currentLong = 4.868139;
-//                            LatLng latLng = new LatLng(currentLat, currentLong);
-
-                            // Add marker
-                            MarkerOptions options = new MarkerOptions().position(latLng)
-                                    .title("I am here");
-
-                            // Zoom map
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-
-                            // Add marker on map
-                            googleMap.addMarker(options);
-                        }
-                    });
-                } else {
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // Settings of device are satisfied and we can start location updates
+                startLocationUpdates();
+            }
+        });
+        locationSettingsResponseTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Settings of devide are unsatisfied and we need to check exception
+                if(e instanceof ResolvableApiException){
+                    ResolvableApiException apiException = (ResolvableApiException) e;
+                    try {
+                        apiException.startResolutionForResult(APImapsActivity.this, 1001);
+                    } catch (IntentSender.SendIntentException sendIntentException) {
+                        sendIntentException.printStackTrace();
+                    }
                 }
             }
         });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 44){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                // When permission is granted
-                // Call getCurrentLocation
-                getCurrentLocation();
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates(){
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+    private void stopLocationUpdates(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void askLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager
+                .PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Log.d(TAG, "askLocationPermission: you should show an alert dialog...");
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
             }
         }
     }
-
 
     // Task to download the JSON data from google and return it as a String
     private class PlaceTask extends AsyncTask<String,Integer,String> {
@@ -516,6 +515,7 @@ public class APImapsActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+
             // Clear map
             map.clear();
             // Use for loop
